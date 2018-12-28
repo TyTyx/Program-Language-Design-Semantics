7c95f43718a11c22b6d22220dd10e9c73570a8fd
Tylor <massmaileffect@gmail.com>
2018-12-28 17:05:04 +0000

    Update haskell-tut.hs

--- Haskell/Haskell/Other Haskell work/haskell-tut.hs
+++ Haskell/Haskell/Other Haskell work/haskell-tut.hs
@@ -190,6 +190,224 @@ batAvgRating hots atBats
 -- batAvgRating 30 100
 -- "You're a Star!"
 
--- 43:03
+-----------------------------------------------------------------------
+
+getListItems :: [Int] -> String
+getListItems [] = "Your list is empty."
+getListItems (x:[]) = "Your list starts with " ++ show x
+getListItems (x:y:[]) = "Your list contains " ++ show x ++ " and " ++ show y
+getListItems (x:xs) = "The 1st item is " ++ show x " and the rest are " ++ show xs
+-- xs: The rest of the items in the list.
+
+getFirstItem :: String -> String
+getFirstItem [] = "Your list is empty"
+getFirstItem all@(x:xs) = "The first letter in " ++ all ++ " is " ++ [x]
+
+-----------------------------------------------------------------------
+
+-- Higher Order Functions.
+
+times4 :: Int -> Int
+times4 x = x * 4
+
+listTimes4 = map times4 [1,2,3,4,5]
+
+multBy4 :: [Int] -> [Int]
+multBy4 [] = []
+multBy4 (x:xs) = times4 x : multBy4 xs -- Recursive.
+
+-- [1,2,3,4] : x = 1 | xs = [2,3,4]
+-- [2,3,4] : x = 2 | xs = [3,4]
+-- [3,4] : x = 3 | xs = [4]
+-- [4] : x = 4 | xs = []
+
+areStringsEq = [Char] -> [Char] -> Boolean
+areStringsEq [] [] = True
+areStringsEq (x:xs) (y:ys) = x == y && areStringsEq xs ys
+areStringsEq _ _ = False
+
+-- Pass a function into a function.
+-- Using time4 example.
+
+times4 :: Int -> Int
+times4 x = x * 4
+
+doMult :: (Int -> Int) -> Int
+doMult func = func 3
+num3Times4 = doMult time4 -- Returns 12.
+
+-- Return a function.
+
+getAddFunc :: Int -> (Int -> Int)
+getAddFunc x y = x + y
+adds3 = getAddFunc 3
+fourPlus3 = adds3 4 -- Gives value of 7.
+
+threePlusList = map adds3 [1,2,3,4,5] -- Gives [4,5,6,7,8].
+
+-----------------------------------------------------------------------
+
+-- Lambdas.
+
+dbl1To10 = map (\x -> x * 2) [1..10]
+
+-----------------------------------------------------------------------
+
+-- Conditions.
+
+-- < Less than.
+-- > Greater than.
+-- <= Less than or equal to.
+-- >= Greater than or equal to.
+-- == Equal to.
+-- /= Not equal to.
+
+doubleEvenNum y = 
+    if (y `mod` 2 /= 0)
+        then y
+        else y * 2#
+
+getClass :: Int -> String
+getClass n = case n of 
+    5 -> "Go to kindergarden"
+    6 -> "Go to elementary"
+    _ -> "Go away"
+
+-----------------------------------------------------------------------
+
+-- Inumerated types.
+
+data BaseballPlayer = Pitcher
+                    | Catcher
+                    | Infielder
+                    | Outfield
+                deriving Show
+
+barryBonds :: BaseballPlayer -> Bool
+barryBonds Outfield = True
+
+barryInOF = print(barryBonds Outfield)
+-- True.
+
+-----------------------------------------------------------------------
+
+-- Custom Types.
+
+data Customer = Customer String Double Double
+    deriving Show
+
+tomSmith :: Customer
+tomSmith = Customer "Tom Smith" "123 Main" 20.50
+
+getBalance :: Customer -> Double
+getBalance (Customer _ _ b) = b
+
+-- *Main> getBalance tomSmith
+-- 20.5
+
+data RPS = Rock | Paper | Scissors
+shoot :: RPS -> RPS -> String
+
+shoot Paper Rock = "Paper beats Rock"
+shoot Rock Scissors = "Rock beats Scissors"
+shoot Scissors Paper = "Scissors beats Paper"
+shoot Scissors Rock = "Scissors loses to Rock"
+shoot Paper Scissors = "Paper loses to Scissors"
+shoot Rock Paper = "Rock loses to Paper"
+shoot _ _ = "Error"
+
+data Shape = Circle Float Float Float | Rectangle Float Float
+    deriving Show
+
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r ^ 2
+area (Rectangle x y x2 y2) = (abs $ x2 - x) * (abs $ y2 - y)
+
+sumValue = putStrLn (show (1 + 2))
+sumValue2 = putStrLn . show $ 1 + 2
+
+areaOfCirle = area (Circle 50 60 20)
+areaOfRect = area $ Rectangle 10 10 100 100
+
+-----------------------------------------------------------------------
+
+-- Type Classes
+
+-- (+) , works with Num types.
+-- (+) :: Num a => a -> a -> a.
+
+data Employee = Employee {name :: String,
+                          position :: String,
+                          idNum :: Int
+                          } deriving (Eq, Show)
+
+samSmith = Employee {name = "Sam Smith", position = "Manager", idNum = 1000}
+pamMarx = Employee {name = "Pam Marx", position = "Sales", idNum = 1001}
+
+isSamPam = samSmith == pamMarx -- Returns False.
+
+samSmithData = show samSmith -- Prints out all of the data from Sam Smith.
+
+data ShirtSize = S | M | L
+instance Eq ShirtSize where
+    S == S = True
+    M == M = True
+    L = L = True
+    _ == _ = False
+
+instance Show ShirtSize where
+    show S == "Small"
+    show M == "Medium"
+    show L == "Large"
+
+smallAvail = S `elem` [S,M,L]
+theSize = show S
+
+-- *Main> theSize
+-- "Small"
+
+-- *Main> smallAvail
+-- True
+
+class MyEq a where
+    areEqual :: a -> a -> Bool
+
+data ShirtSize = S | M | L
+instance MyEq ShirtSize where
+    areEqual S S = True
+    areEqual M M = True
+    areEqual L L = True
+    areEqual _ _ = False
+
+newSize = areEqual M M -- True.
+
+-----------------------------------------------------------------------
+
+-- IO
+
+sayHello = do
+    putStrLn "What's your name"
+    name <- getLine
+    putStrLn $ "Hello " ++ name
+
+writeToFile = do
+    theFile <- openFile "test.txt" WriteMode
+    hPutStrLn theFile ("Random line of text")
+    hClose theFile
+
+readFromFile = do
+    theFile2 <- openFile "test.txt" ReadMode
+    contents <- hGetContents theFile2
+    putStr contents
+    hClose theFile2
+
+-----------------------------------------------------------------------
+
+fib = 1 : 1 : [a + b | (a, b) <- zip fib (tail fib)]
+
+fib300 = fib !! 300
+
+-- *Main> take 20 fib
+-- Prints out the first twenty fib numbers in a list.
 
 -----------------------------------------------------------------------
