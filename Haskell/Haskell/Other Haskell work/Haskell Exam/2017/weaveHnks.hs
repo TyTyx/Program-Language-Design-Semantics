550a9a602c495a3fb4437e3a3820862bf75058be
Tylor <massmaileffect@gmail.com>
2019-01-07 09:35:55 +0000

    Update weaveHnks.hs

--- Haskell/Haskell/Other Haskell work/Haskell Exam/2017/weaveHnks.hs
+++ Haskell/Haskell/Other Haskell work/Haskell Exam/2017/weaveHnks.hs
@@ -1,6 +1,12 @@
-count :: [Int] -> [Int] -> [Int]
-count (x:xs:y:ys) = [x] [y] ++ count [xs] [ys]
+nheads :: Int -> [a] -> [a]
+nheads n [] = []
+nheads n (x:xs) = if n == 1 then x:[] else x:nheads (n - 1) xs
 
--- weaveHunks :: Int -> List -> List
-weaveHunks :: [Int] -> [Int] -> [Int]
-weaveHunks xs ys = (count xs ys)
+ntails :: Int -> [a] -> [a]
+ntails n [] = []
+ntails n (x:xs) = if n == 1 then xs else x:ntails (n - 1) xs
+
+weaveHunks :: Int -> [a] -> [a] -> [a]
+weaveHunks n xs [] = xs
+weaveHunks n [] ys = ys
+weaveHunks n xs ys = nheads n xs ++ nheads n ys ++ weaveHunks n (ntails n xs) (ntails n ys)
