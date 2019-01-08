6219246324735e1e883a33451f91748273cdd431
Tylor <massmaileffect@gmail.com>
2019-01-08 13:53:06 +0000

    Update january-18.rkt

--- Scheme/Scheme Exam/january-18.rkt
+++ Scheme/Scheme Exam/january-18.rkt
@@ -7,8 +7,9 @@
     (cond ((null? x) x)
           ((pair? (car x)) (append (foo (car x)) (foo (cdr x))))
           (else (append (foo (car x)) (foo (cdr x)))))))
-;;(else (append (foo (car x)) (foo (cdr x))))
 
 (define tear
   (lambda (f x)
     (filter f (foo x))))
+
+;; TODO: Place the other elements that don't meet the cond into a list.
