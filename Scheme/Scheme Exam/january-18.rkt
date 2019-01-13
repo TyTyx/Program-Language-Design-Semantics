edb75c2463a4f0a849d7551c844fa8be41e2cc86
Tylor <massmaileffect@gmail.com>
2019-01-13 11:15:53 +0000

    Update january-18.rkt

--- Scheme/Scheme Exam/january-18.rkt
+++ Scheme/Scheme Exam/january-18.rkt
@@ -2,14 +2,8 @@
 
 ;; January 2018 CS424
 
-(define foo
-  (lambda (x)
-    (cond ((null? x) x)
-          ((pair? (car x)) (append (foo (car x)) (foo (cdr x))))
-          (else (append (foo (car x)) (foo (cdr x)))))))
-
 (define tear
   (lambda (f x)
-    (filter f (foo x))))
+    (list (filter (f x))))
 
 ;; TODO: Place the other elements that don't meet the cond into a list.
