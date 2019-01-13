b46860e19a607a748bf7524765e40ef123e81c6f
Tylor <massmaileffect@gmail.com>
2019-01-13 11:29:53 +0000

    Update january-18.rkt

--- Scheme/Scheme Exam/january-18.rkt
+++ Scheme/Scheme Exam/january-18.rkt
@@ -1,9 +1,6 @@
 #lang racket
-
-;; January 2018 CS424
-
 (define tear
   (lambda (f x)
-    (list (filter (f x))))
+    (list (filter f x) (not (filter f x)))))
 
-;; TODO: Place the other elements that don't meet the cond into a list.
+;; TODO: Place the other elements that don't meet the cond into a list. Currrently returns '#f' instead of elements themselves.
